8e1d30ce9edb0823fa12c95f7d9dc74cbe756052
Roy <roytol12@gmail.com>
2024-10-16 16:24:13 +0300

    DarkWeb scanner service and the controller implemented.

--- part2/src/main/java/com/enqode/part2/controller/ServiceController.java
+++ part2/src/main/java/com/enqode/part2/controller/ServiceController.java
@@ -1,13 +1,24 @@
 package com.enqode.part2.controller;
 
+import com.enqode.part2.service.DarkWebScannerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/darkweb")
 @RequiredArgsConstructor
 @Slf4j
 public class ServiceController {
+    private DarkWebScannerService darkWebScannerService;
+
+    @GetMapping("/scan")
+    public Mono<ResponseEntity<String>> scanDomain(@RequestParam String domain) {
+        return darkWebScannerService.scanDomain(domain);
+    }
 }
--- part2/src/main/java/com/enqode/part2/service/DarkWebScannerService.java
+++ part2/src/main/java/com/enqode/part2/service/DarkWebScannerService.java
@@ -1,7 +1,16 @@
 package com.enqode.part2.service;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
 public class DarkWebScannerService {
+    private IntelXService intelXService;
+
+    public Mono<ResponseEntity<String>> scanDomain(String domain) {
+        return intelXService.searchDomain(domain)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
 }
