2200d5cd87d59881a6e6c281a1b6fd8e89fe19c9
Roy <roytol12@gmail.com>
2024-10-16 16:19:56 +0300

    intelX service created

--- part2/src/main/java/com/enqode/part2/service/IntelXService.java
+++ part2/src/main/java/com/enqode/part2/service/IntelXService.java
@@ -1,7 +1,32 @@
 package com.enqode.part2.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Service
 public class IntelXService {
+    private final WebClient webClient;
+
+    @Value("${intelx.api.key}")
+    private String apiKey;
+
+    @Value("${intelx.api.url}")
+    private String apiUrl;
+
+    public IntelXService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+    }
+
+    public Mono<String> searchDomain(String domain) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(apiUrl + "/intelligent/search")
+                        .queryParam("term", domain)
+                        .queryParam("key", apiKey)
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class);
+    }
 }
