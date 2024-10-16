46a7772af0f317287f70e7125c8cd7c0021e882d
Roy <roytol12@gmail.com>
2024-10-16 16:17:36 +0300

    init

---
+++ part2/src/main/java/com/enqode/part2/controller/ServiceController.java
@@ -0,0 +1,13 @@
+package com.enqode.part2.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/darkweb")
+@RequiredArgsConstructor
+@Slf4j
+public class ServiceController {
+}
